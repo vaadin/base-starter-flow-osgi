5026ee3f6bd126d068179444ede09bd6c4de505e
Denis Anisimov <denis@vaadin.com>
2018-11-26 16:59:59 +0300

    Some improvements due code review.

--- src/main/java/com/example/starter/base/osgi/VaadinServletRegistration.java
+++ src/main/java/com/example/starter/base/osgi/VaadinServletRegistration.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2000-2018 Vaadin Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
 package com.example.starter.base.osgi;
 
 import java.util.Hashtable;
@@ -35,7 +20,8 @@ import com.vaadin.flow.server.VaadinServlet;
 public class VaadinServletRegistration {
 
     /**
-     * This class is a workaround for #4367. It won't be needed once it's fixed
+     * This class is a workaround for #4367. This will be removed after the
+     * issue is fixed.
      */
     private static class FixedVaadinServlet extends VaadinServlet {
         @Override
