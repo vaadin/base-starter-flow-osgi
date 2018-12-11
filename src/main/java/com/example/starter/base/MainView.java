a5373deee2d662697892ed2584e324f1b75f5d35
Pekka <pekka@vaadin.com>
2018-12-11 15:11:29 +0200

    Use platform BOM for versions
    Includes a workaround until vaadin/flow/issues/4847 is fixed.

--- src/main/java/com/example/starter/base/MainView.java
+++ src/main/java/com/example/starter/base/MainView.java
@@ -5,11 +5,15 @@ import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  * The main view contains a button and a click listener.
  */
 @Route("")
+// Need to explicitly declare the Lumo until https://github.com/vaadin/flow/issues/4847 is fixed
+@Theme(Lumo.class)
 @PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
 public class MainView extends VerticalLayout {
 
