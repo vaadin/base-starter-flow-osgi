e241adf8c75237bb08e65a582f28af7749d72e39
Mehdi Javan <mehdi@vaadin.com>
2019-01-10 10:50:41 +0200

    Create another bundle for testing purpose

--- src/main/java/com/example/starter/anotherbundle/AnotherView.java
+++ src/main/java/com/example/starter/anotherbundle/AnotherView.java
@@ -1,25 +1,23 @@
-package com.example.starter.base;
+package com.example.starter.anotherbundle;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  * The main view contains a button and a click listener.
  */
-@Route("")
+@Route("another")
 // Need to explicitly declare the Lumo until https://github.com/vaadin/flow/issues/4847 is fixed
 @Theme(Lumo.class)
-@PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
-public class MainView extends VerticalLayout {
+public class AnotherView extends VerticalLayout {
 
-    public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
+    public AnotherView() {
+        Button button = new Button("Click me in another view",
+                event -> Notification.show("Clicked in another view!"));
         add(button);
     }
 }
