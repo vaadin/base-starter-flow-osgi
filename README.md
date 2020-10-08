949f489e3959df6058f663785e56a1b5c6e3aec8
Denis Anisimov <denis@vaadin.com>
2020-10-08 18:20:59 +0300

    Make it possible to run NPM mode in OSGi container

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Base Starter for Vaadin Flow and OSGi
+# Base Starter for Vaadin Flow and OSGi in NPM mode
 
 This project can be used as a starting point to create your own Vaadin Flow application bundle for OSGi.
 It has the necessary dependencies and files to help you get started.
@@ -7,10 +7,10 @@ The best way to use it is via [vaadin.com/start](https://vaadin.com/start) - you
 
 To access it directly from github, clone the repository and import the project to the IDE of your choice as a Maven project. You need to have Java 8 or 11 installed.
 
-Run using `mvn -Pprepare-osgi-container -Prun-osgi-container verify` and open [http://localhost:8080](http://localhost:8080) in browser.
+Run using `mvn -Pprepare-osgi-container -Prun-osgi-container -Pproduction verify` and open [http://localhost:8080](http://localhost:8080) in browser.
 This command will run profiles which downloads for you Felix OSGi container and installs `felix-jetty` 
 as OSGi web container. It deploys all necessary bundles to Felix and start it synchronously.
 The container will be stopped  when you stop your maven build (e.g. by killing it via `Ctrl+C`).
 
 :warning:
-At the moment, Vaadin 14 supports OSGi only in compatibility mode. 
+At the moment, Vaadin 14 supports OSGi only in production mode. 
--- src/main/java/com/example/starter/base/osgi/VaadinServletRegistration.java
+++ src/main/java/com/example/starter/base/osgi/VaadinServletRegistration.java
@@ -1,7 +1,7 @@
 package com.example.starter.base.osgi;
 
-import com.vaadin.flow.function.DeploymentConfiguration;
 import java.util.Hashtable;
+import java.util.Properties;
 
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
@@ -10,18 +10,24 @@ import javax.servlet.ServletException;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.http.HttpService;
+import org.osgi.service.http.NamespaceException;
 import org.osgi.service.http.whiteboard.HttpWhiteboardConstants;
 
+import com.vaadin.flow.function.DeploymentConfiguration;
+import com.vaadin.flow.server.DefaultDeploymentConfiguration;
+import com.vaadin.flow.server.DeploymentConfigurationFactory;
+import com.vaadin.flow.server.InitParameters;
 import com.vaadin.flow.server.VaadinServlet;
-import com.vaadin.flow.server.VaadinServletConfiguration;
-import java.util.Properties;
-import javax.servlet.annotation.WebServlet;
 
 /**
  * Register a VaadinServlet via HTTP Whiteboard specification
  */
 @Component(immediate = true)
 public class VaadinServletRegistration {
+
+    private HttpService httpService;
 
     /**
      * This class is a workaround for #4367. This will be removed after the
@@ -36,24 +42,61 @@ public class VaadinServletRegistration {
         }
 
         @Override
-        protected DeploymentConfiguration createDeploymentConfiguration(Properties initParameters) {
-            // npm mode is not currently supported
-            initParameters.setProperty("compatibilityMode", "true");
-            return super.createDeploymentConfiguration(initParameters);
+        protected DeploymentConfiguration createDeploymentConfiguration(
+                Properties initParameters) {
+            initParameters.remove(
+                    DeploymentConfigurationFactory.DEV_MODE_ENABLE_STRATEGY);
+            initParameters.put(InitParameters.SERVLET_PARAMETER_PRODUCTION_MODE,
+                    true);
+            DeploymentConfiguration config = new DefaultDeploymentConfiguration(
+                    getClass(), initParameters) {
+                @Override
+                public boolean isProductionMode() {
+                    return true;
+                }
+
+                @Override
+                public boolean isStatsExternal() {
+                    return true;
+                }
+
+                @Override
+                public String getExternalStatsUrl() {
+                    return "/VAADIN/config/stats.json";
+                }
+            };
+
+            return config;
         }
-        
+
     }
 
     @Activate
-    void activate(BundleContext ctx) {
+    void activate(BundleContext ctx) throws NamespaceException {
         Hashtable<String, Object> properties = new Hashtable<>();
         properties.put(
                 HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_ASYNC_SUPPORTED,
                 true);
+        properties.put(InitParameters.SERVLET_PARAMETER_COMPATIBILITY_MODE,
+                false);
+        properties.put(InitParameters.SERVLET_PARAMETER_PRODUCTION_MODE, true);
         properties.put(HttpWhiteboardConstants.HTTP_WHITEBOARD_SERVLET_PATTERN,
                 "/*");
         ctx.registerService(Servlet.class, new FixedVaadinServlet(),
                 properties);
+
+        httpService.registerResources("/VAADIN/config/stats.json",
+                "/META-INF/VAADIN/config/stats.json",
+                httpService.createDefaultHttpContext());
+    }
+
+    @Reference
+    void setHttpService(HttpService service) {
+        this.httpService = service;
+    }
+
+    void unsetHttpService(HttpService service) {
+        this.httpService = null;
     }
 
 }
