000f646ba1e1917f844ab589b0104b1df396780a
caalador <mikael.grankvist@gmail.com>
2021-01-12 13:38:50 +0200

    Add custom theme to starter (#104)
    Part of vaadin/flow#9597
    
    Co-authored-by: Denis <denis@vaadin.com>

--- starter/src/main/java/com/example/starter/base/MainView.java
+++ starter/src/main/java/com/example/starter/base/MainView.java
@@ -1,19 +1,41 @@
 package com.example.starter.base;
 
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
 /**
- * The main view contains a button and a click listener.
+ * The main view contains a text field for getting the user name and a button
+ * that shows a greeting message in a notification.
  */
 @Route("")
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(button);
+        // Use TextField for standard text input
+        TextField textField = new TextField("Your name");
+        textField.addThemeName("bordered");
+        // Button click listeners can be defined as lambda expressions
+        GreetService greetService = new GreetService();
+        Button button = new Button("Say hello", e -> Notification
+                .show(greetService.greet(textField.getValue())));
+
+        // Theme variants give you predefined extra styles for components.
+        // Example: Primary button is more prominent look.
+        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        // You can specify keyboard shortcuts for buttons.
+        // Example: Pressing enter in this view clicks the Button.
+        button.addClickShortcut(Key.ENTER);
+
+        // Use custom CSS classes to apply styling. This is defined in
+        // shared-styles.css.
+        addClassName("centered-content");
+
+        add(textField, button);
     }
 }
